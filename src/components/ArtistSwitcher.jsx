e885fb33cc46c65c6b22701ca2bd32c4239e9f00
Zijun Wang <zijunwang2020@gmail.com>
2025-01-15 07:22:59 +0800

    added artist texts

--- src/components/ArtistSwitcher.jsx
+++ src/components/ArtistSwitcher.jsx
@@ -3,9 +3,13 @@ import artist_0 from "../assets/images/artists/artist_0.png";
 import artist_1 from "../assets/images/artists/artist_1.png";
 import artist_2 from "../assets/images/artists/artist_2.png";
 import artist_3 from "../assets/images/artists/artist_3.png";
+import aText_0 from "../assets/images/artists-text/atext_0.png";
+import aText_1 from "../assets/images/artists-text/atext_1.png";
+import aText_2 from "../assets/images/artists-text/atext_2.png";
 
 const ArtistSwitcher = ({ currentIndex, onSwipe }) => {
 	const artists = [artist_0, artist_1, artist_2, artist_3];
+	const aTexts = [aText_0, aText_1, aText_2];
 
 	const handleDragEnd = (event, info) => {
 		const swipeThreshold = 50; // minimum distance for swipe
@@ -41,6 +45,11 @@ const ArtistSwitcher = ({ currentIndex, onSwipe }) => {
 					</div>
 				))}
 			</motion.div>
+			<div className="artist-text">
+				{aTexts[currentIndex] && (
+					<img src={aTexts[currentIndex]} alt={`Artist Text ${currentIndex}`} />
+				)}{" "}
+			</div>
 		</div>
 	);
 };
